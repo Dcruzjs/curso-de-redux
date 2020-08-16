b53ea2b1eb4da4793c2f92fa6092ffbbf807c5fb
Daniel Cruz <dcruz.js@gmail.com>
2020-08-15 22:14:20 -0600

    Updates

---
+++ precache-manifest.2054538f6ff54990e5d4aa0acbd8e84b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf4e9c3d36d31e6beba2526b66a043e4",
+    "url": "/curso-de-redux/index.html"
+  },
+  {
+    "revision": "5cea863529899039c7d8",
+    "url": "/curso-de-redux/static/css/main.cf54eb25.chunk.css"
+  },
+  {
+    "revision": "9d5622776c903e72e7bf",
+    "url": "/curso-de-redux/static/js/2.bfecfe1a.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/curso-de-redux/static/js/2.bfecfe1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cea863529899039c7d8",
+    "url": "/curso-de-redux/static/js/main.a81c9cf0.chunk.js"
+  },
+  {
+    "revision": "00b0993bb8fa8f475d01",
+    "url": "/curso-de-redux/static/js/runtime-main.b08f2e4a.js"
+  }
+]);
